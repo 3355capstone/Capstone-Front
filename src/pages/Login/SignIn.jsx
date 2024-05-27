08a9b84a6cf1ee4626b4c4f81075f05030d47581
SangYoonLee <prestro1231@hufs.ac.kr>
2024-05-27 16:47:55 +0900

    Modify: 로그인/회원가입 연동 코드 JWT 토큰 방식에 맞춰 1차 수정 완료

--- src/pages/Login/SignIn.jsx
+++ src/pages/Login/SignIn.jsx
@@ -1,27 +1,30 @@
 // Login.js
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { login } from "../api/AuthAPI";
 import styled from "styled-components";
 
 const SignIn = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
 
   const [isEmailError, setIsEmailError] = useState(false);
   const emailRegExp =
     /^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
   const checkEmailForm = () => {
-    if (emailRegExp.test(email)) {
+    if (emailRegExp.test(values.email)) {
       setIsEmailError(false);
     } else {
       setIsEmailError(true);
     }
   };
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
 
     // 이메일이 올바른 형태인지 검사
@@ -32,33 +35,47 @@ const SignIn = () => {
     }
 
     // 비밀번호가 공란인지 검사
-    if (password === "") {
+    if (values.password === "") {
       alert("로그인 실패 (시유: 비밀번호 혹은 닉네임이 공란임)");
     }
 
     // 백엔드와 통신
-    fetch("http://서버주소/user/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    })
+    // fetch("http://서버주소/user/login", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json;charset=utf-8",
+    //   },
+    //   body: JSON.stringify({
+    //     email: email,
+    //     password: password,
+    //   }),
+    // })
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     if (data.token) {
+    //       localStorage.setItem("TOKEN", data.accessToken);
+    //       navigate("/");
+    //     } else {
+    //       alert(
+    //         "로그인 실패 (이메일 혹은 비밀번호가 존재하지 않거나 올바르지 않음)"
+    //       );
+    //     }
+    //   });
+    login(values)
       .then((response) => {
-        return response.json();
+        localStorage.clear();
+        localStorage.setItem("tokenType", response.tokenType);
+        localStorage.setItem("accessToken", response.accessToken);
+        localStorage.setItem("refreshToken", response.refreshToken);
+        window.location.href = `/`;
       })
-      .then((data) => {
-        if (data.token) {
-          localStorage.setItem("TOKEN", data.accessToken);
-          navigate("/");
-        } else {
-          alert(
-            "로그인 실패 (이메일 혹은 비밀번호가 존재하지 않거나 올바르지 않음)"
-          );
-        }
+      .catch((error) => {
+        console.log(error);
+        alert(
+          "로그인 실패 (이메일 혹은 비밀번호가 존재하지 않거나 올바르지 않음)"
+        );
       });
   };
 
@@ -68,14 +85,24 @@ const SignIn = () => {
       <Input
         type="text"
         placeholder="이메일"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={values.email}
+        onChange={async (e) =>
+          setValues((prevState) => ({
+            ...prevState,
+            email: e.target.value,
+          }))
+        }
       />
       <Input
         type="password"
         placeholder="비밀번호"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        value={values.password}
+        onChange={async (e) =>
+          setValues((prevState) => ({
+            ...prevState,
+            password: e.target.value,
+          }))
+        }
       />
       <LoginBtn onClick={handleLogin}>로그인</LoginBtn>
     </Container>
